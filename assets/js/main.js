b04ee6e347bc4ea56283bea7531c3e1cdfe038df
saidmsaif <saidmsaiff@gmail.com>
2021-12-01 12:56:26 +0600

    remove owl carousel file

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,8 +42,6 @@ $(document).ready(function () {
 				settings: {
 					slidesToShow: 4,
 					slidesToScroll: 2,
-					infinite: true,
-					dots: true,
 				},
 			},
 			{
@@ -51,8 +49,6 @@ $(document).ready(function () {
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 3,
-					infinite: true,
-					dots: true,
 				},
 			},
 			{
