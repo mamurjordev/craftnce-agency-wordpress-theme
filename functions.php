7187b04c28ded414ceb7a7ef8aab14e00acdf332
saidmsaif <saidmsaiff@gmail.com>
2021-12-04 22:41:34 +0600

    add styles

--- functions.php
+++ functions.php
@@ -14,3 +14,17 @@
         add_theme_support('custom-background');
     }
     add_action('after_setup_theme', 'craftnce_theme_setup');
+
+    function craftnce_assets_enqueue() {
+        // Stylesheets Enqueue
+        wp_enqueue_style('craftnce-bootstrap-css', '//cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css');
+        wp_enqueue_style('craftnce-fontawesome-css', '//use.fontawesome.com/releases/v5.15.4/css/all.css');
+        wp_enqueue_style('craftnce-remixicon-css', '//cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css');
+        wp_enqueue_style('craftnce-google-fonts', '//fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+        wp_enqueue_style('craftnce-slick-slider-css', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+        wp_enqueue_style('craftnce-slick-nav-css', get_template_directory_uri().'/assets/css/slicknav-1.0.10.min.css');
+        wp_enqueue_style('craftnce-main-style', get_template_directory_uri().'/assets/css/style.css');
+        wp_enqueue_style('craftnce-responsive-style', get_template_directory_uri().'/assets/css/responsive.css');
+        wp_enqueue_style('theme-default-css', get_stylesheet_uri());
+    }
+    add_action('wp_enqueue_scripts', 'craftnce_assets_enqueue');
