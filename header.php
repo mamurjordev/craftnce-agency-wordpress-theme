18973e45ae631274b2c47478bc66b087622b673a
saidmsaif <saidmsaiff@gmail.com>
2021-12-04 22:50:24 +0600

    minor changes

--- header.php
+++ header.php
@@ -3,10 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/x-icon" href="">
     
-    <title><?php bloginfo( 'title-tag' ); ?></title>
-
+    <?php bloginfo( 'title-tag' ); ?>
     <?php wp_head(); ?>
 </head>
 <body class="<?php body_class(); ?>">
