1f93689a882f4a524224f893c25e53355eba1e6f
saidmsaif <saidmsaiff@gmail.com>
2021-12-06 21:19:44 +0600

    counter section

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -169,40 +169,48 @@
     </section>
     <?php
         endif;
+        if(get_craftnce_data('is_show_counter_section')) :
     ?>
 
     <!-- Counter Section -->
     <section class="counter-bg">
         <div class="counter-overlay py-50">
             <div class="container text-center py-5">
-                <h6 class="text-uppercase text-md fw-bold counter-section-subtitle">featured case study</h6>
-                <h2 class="text-uppercase text-light fw-900 text-xl mt-3">Helping dreams to do</h2>
+                <h6 class="text-uppercase text-md fw-bold counter-section-subtitle">
+                    <?php
+                        echo get_craftnce_data('service_counter_subtitle');
+                    ?>
+                </h6>
+                <h2 class="text-uppercase text-light fw-900 text-xl mt-3">
+                    <?php
+                        echo get_craftnce_data('service_counter_title');
+                    ?>
+                </h2>
 
                 <div class="row mt-5">
+                    <?php
+                        $counter_items = get_craftnce_data('counter_items');
+                        $_counter = count((array)$counter_items);
+                        
+                        for($i = 0; $i < $_counter; $i++) :
+                    ?>
                     <div class="col-sm-6 col-lg-3 text-uppercase">
-                        <i class="ri-code-s-slash-line text-warning fs-48"></i>
-                        <h2 class="text-uppercase text-light fw-bold fs-48">200</h2>
-                        <p class="text-white">Lines of code</p>
+                        <i class="<?php echo esc_attr($counter_items[$i]['counter_icon']); ?> text-warning fs-48 mb-3"></i>
+                        <h2 class="text-uppercase text-light fw-900 fs-48">
+                            <?php echo $counter_items[$i]['countable_numbers']; ?>
+                        </h2>
+                        <p class="text-white">
+                            <?php echo $counter_items[$i]['counter_title']; ?>
+                        </p>
                     </div>
-                    <div class="col-sm-6 col-lg-3 text-uppercase">
-                        <i class="ri-user-line text-warning fs-48"></i>
-                        <h2 class="text-uppercase text-light fw-bold fs-48">100+</h2>
-                        <p class="text-white">Happy clients</p>
-                    </div>
-                    <div class="col-sm-6 col-lg-3 text-uppercase">
-                        <i class="ri-code-s-slash-line text-warning fs-48"></i>
-                        <h2 class="text-uppercase text-light fw-bold fs-48">250+</h2>
-                        <p class="text-white">projects</p>
-                    </div>
-                    <div class="col-sm-6 col-lg-3 text-uppercase">
-                        <i class="ri-award-fill text-warning fs-48"></i>
-                        <h2 class="text-uppercase text-light fw-bold fs-48">30</h2>
-                        <p class="text-white">awards</p>
-                    </div>
+                    <?php endfor; ?>
                 </div>
             </div>
         </div>
     </section>
+    <?php
+        endif;
+    ?>
 
     <!-- Testimonial Section -->
     <section>
