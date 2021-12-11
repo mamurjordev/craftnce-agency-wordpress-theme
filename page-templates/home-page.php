7c919752d2229a72966faf018c63ef0ae714fb1a
saidmsaif <saidmsaiff@gmail.com>
2021-12-11 12:16:43 +0600

    service repeater and repeater issue fix

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -111,22 +111,29 @@
         <div class="container py-4 py-lg-5">
             <div class="row">
                 <?php
-                    $features_items = get_craftnce_data('features_items');
-                    $_count = count((array)$features_items);
 
-                    for($i = 0; $i < $_count; $i++) :
+                    $craftnce_features_repeater = get_theme_mod('craftnce_home_features_item_settings');
+                    $craftnce_features_repeater_decoded = json_decode($craftnce_features_repeater);
+                    if(!empty($craftnce_features_repeater_decoded)) :
+                        foreach($craftnce_features_repeater_decoded as $repeater_item) :
                 ?>
+
                 <div class="col-sm-6 col-lg-3 mt-2">
                     <div class="features-box d-flex">
-                        <i class="<?php echo esc_attr($features_items[$i]['features_item_icon_class']); ?> fs-4 me-3 mt-1"></i>
+                        <i class="<?php echo esc_attr($repeater_item->icon_value); ?> fs-4 me-3 mt-1"></i>
                         <h5 class="features-item-title">
                             <?php
-                                echo $features_items[$i]['features_item_title'];
+                                echo $repeater_item->title;
                             ?>
                         </h5>
                     </div>
                 </div>
-                <?php endfor; ?>
+
+                <?php
+                        endforeach;
+                    endif;
+                ?>
+                
             </div>
         </div>
     </section>
