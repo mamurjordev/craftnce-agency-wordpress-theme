f4417f493883e967bc11de7b7a63737dc0371c7d
saidmsaif <saidmsaiff@gmail.com>
2021-12-12 09:18:45 +0600

    add dynamic logo

--- header.php
+++ header.php
@@ -14,7 +14,16 @@
         <nav class="navbar navbar-expand-lg navbar-light py-3">
             <div class="container">
                 <a class="navbar-brand text-primary fw-bolder" href="<?php echo home_url(); ?>">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="img-fluid" alt="">
+                    <!-- <img src="<?php //echo get_template_directory_uri(); ?>/assets/img/logo.png" class="img-fluid" alt=""> -->
+                    <?php
+                        if(current_theme_supports('custom-log')) {
+                            the_custom_logo('large', array('class'=>'img-fluid'));
+                        } else {
+                    ?>
+                    <h4 class="mb-0"><?php bloginfo('title-tag'); ?></h4>
+                    <?php
+                        }
+                    ?>
                 </a>
     
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
