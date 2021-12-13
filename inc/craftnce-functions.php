4fc0e1b774bdc2137cecbbff49d9382f2eaf1b63
saidmsaif <saidmsaiff@gmail.com>
2021-12-13 12:58:36 +0600

    css issue fix

--- inc/craftnce-functions.php
+++ inc/craftnce-functions.php
@@ -8,7 +8,7 @@
                 --secondary-color: <?php echo sanitize_hex_color(get_theme_mod('craftnce_secondary_color_settings', '#EDF5FB')); ?>;
                 --section-bg: <?php echo sanitize_hex_color(get_theme_mod('craftnce_section_bg_color_settings', '#082032')); ?>;
                 --footer-bg: <?php echo sanitize_hex_color(get_theme_mod('craftnce_footer_bg_color_settings', '#082032')); ?>;
-                --overlay: <?php echo sanitize_hex_color(get_theme_mod('craftnce_overlay_color_settings'), '#082032d6'); ?>;
+                --overlay: <?php echo sanitize_hex_color(get_theme_mod('craftnce_overlay_color_settings'), '#082032'); ?>;
 
                 --orange-color: #FFC700;
                 --primary-gray: #dedede;
