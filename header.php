b753d566fab25178ed0e5732928de32dc5c3b84f
saidmsaif <saidmsaiff@gmail.com>
2021-12-13 09:34:02 +0600

    menu fallback remove

--- header.php
+++ header.php
@@ -42,7 +42,6 @@
                             'theme_location'            =>  'primary-menu',
                             'menu_class'                =>  '',
                             'menu-container'            =>  'false',
-                            'fallback_cb' => '__return_false',
                             'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-lg-0 text-sm %2$s">%3$s</ul>',
                             'depth' => 2,
                             'walker' => new bootstrap_5_wp_nav_menu_walker(),
