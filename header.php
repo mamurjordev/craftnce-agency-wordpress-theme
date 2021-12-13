217b60fa23db1df7aae4d79da5e2e6c8a791b7a6
saidmsaif <saidmsaiff@gmail.com>
2021-12-13 09:46:15 +0600

    fix header issue

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <body class="<?php body_class(); ?>">
     <?php wp_body_open(); ?>
     <!-- Header Navigation -->
-    <header id="site-header" class="sticky-top bg-light <?php if(is_page() && !is_front_page() && is_home()) { echo esc_attr('shadow-sm'); } ?>">
+    <header id="site-header" class="sticky-top bg-light">
         <nav class="navbar navbar-expand-lg navbar-light py-2">
             <div class="container">
                 <a class="navbar-brand text-primary fw-bolder" href="<?php echo home_url(); ?>">
