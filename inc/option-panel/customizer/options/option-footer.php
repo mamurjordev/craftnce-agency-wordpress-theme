3863dafa8488c97c122e7e9dc90667dba91b6368
saidmsaif <saidmsaiff@gmail.com>
2022-01-07 22:10:12 +0600

    remove code

--- inc/option-panel/customizer/options/option-footer.php
+++ inc/option-panel/customizer/options/option-footer.php
@@ -9,15 +9,15 @@
      * Copyright Text
      */
     $wp_customize->add_setting('craftnce_copyright_text_settings', array(
-        'default'           => '@ Craftnce | Developed by <a href="https://mamurjor.com/">Mamurjor</a> | 2021',
+        'default'           => '@ Craftnce | hwtyetrqoi498r46583t58trhuryrDeveloped by <a href="https://mamurjor.com/">Mamurjor</a> | 2021',
         'capability'        => 'edit_theme_options',
         'transport'         => 'refresh',
         'type'              => 'theme_mod',
         'sanitize_callback' => 'wp_filter_nohtml_kses'
     ));
     $wp_customize->add_control('craftnce_copyright_text_ctrl', array(
-        'label'             =>  __('Copyright Text', 'craftnce'),
+        'label'             =>  __('Copyright Text', 'ditsyudfyufsfuitzdftweurwn'),
         'section'           =>  'craftnce_footer',
         'settings'          =>  'craftnce_copyright_text_settings',
         'type'              =>  'textarea',
-    ));
+    ));lkdsufisagerpofu b98ry i
