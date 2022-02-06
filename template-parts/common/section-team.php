5bd25dab57d748a357b3af8973b65d10620f9d38
saidmsaif <saidmsaiff@gmail.com>
2022-02-06 11:02:54 +0600

    change team section markup

--- template-parts/common/section-team.php
+++ template-parts/common/section-team.php
@@ -1,57 +1,100 @@
-<?php
-    $craftnce_team_member_settings_repeater = get_theme_mod('craftnce_team_section_item_settings');
-    $craftnce_team_member_settings_repeater_decoded = json_decode($craftnce_team_member_settings_repeater);
-    if(!empty($craftnce_team_member_settings_repeater_decoded)) :
-?>
 <!-- Team Section -->
-<section class="team-section py-5">
+<section class="team-sction py-5">
     <div class="container py-5">
         <div class="row text-center">
             <div class="col-sm-7 col-xl-4 mx-auto">
-                <h2 class="fw-900">
-                    <?php
-                        echo get_craftnce_data('team_section_title');
-                    ?>
-                </h2>
+                <h2 class="fw-900">Meet Our Experienced & Skilled Team</h2>
             </div>
         </div>
         <div class="row mt-4">
-            <?php
-                foreach($craftnce_team_member_settings_repeater_decoded as $craftnce_team_member) :
-            ?>
             <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
                 <div class="team-member-card card border-0 overflow-hidden d-flex flex-column justify-content-between">
                     <div class="team-avatar text-center">
-                        <img src="<?php echo esc_url($craftnce_team_member->image_url); ?>" class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/images.png"
+                            class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
                     </div>
 
                     <div class="text-center">
-                        <h3>
-                            <?php
-                                echo $craftnce_team_member->title;
-                            ?>
-                        </h3>
-                        <span class="text-sm fw-light">
-                            <?php
-                                echo $craftnce_team_member->text;
-                            ?>
-                        </span>
+                        <h3>Hasan Ali</h3>
+                        <h6>Founder & Chairman</h6>
                     </div>
 
                     <div class="team-social-container bg-primary py-2">
                         <div class="team-social w-max mx-auto">
-                            <a href="">
-                                <i class=""></i>
-                            </a>
+                            <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#!"><i class="fab fa-instagram"></i></a>
+                            <a href="#!"><i class="fab fa-twitter"></i></a>
+                            <a href="#!"><i class="fab fa-whatsapp"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <?php
-                endforeach;
-            ?>
+            <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
+                <div class="team-member-card card border-0 overflow-hidden d-flex flex-column justify-content-between">
+                    <div class="team-avatar text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/images.png"
+                            class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
+                    </div>
+
+                    <div class="text-center">
+                        <h3>Hasan Ali</h3>
+                        <h6>Founder & Chairman</h6>
+                    </div>
+
+                    <div class="team-social-container bg-primary py-2">
+                        <div class="team-social w-max mx-auto">
+                            <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#!"><i class="fab fa-instagram"></i></a>
+                            <a href="#!"><i class="fab fa-twitter"></i></a>
+                            <a href="#!"><i class="fab fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
+                <div class="team-member-card card border-0 overflow-hidden d-flex flex-column justify-content-between">
+                    <div class="team-avatar text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/images.png"
+                            class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
+                    </div>
+
+                    <div class="text-center">
+                        <h3>Hasan Ali</h3>
+                        <h6>Founder & Chairman</h6>
+                    </div>
+
+                    <div class="team-social-container bg-primary py-2">
+                        <div class="team-social w-max mx-auto">
+                            <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#!"><i class="fab fa-instagram"></i></a>
+                            <a href="#!"><i class="fab fa-twitter"></i></a>
+                            <a href="#!"><i class="fab fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
+                <div class="team-member-card card border-0 overflow-hidden d-flex flex-column justify-content-between">
+                    <div class="team-avatar text-center">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/images.png"
+                            class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
+                    </div>
+
+                    <div class="text-center">
+                        <h3>Hasan Ali</h3>
+                        <h6>Founder & Chairman</h6>
+                    </div>
+
+                    <div class="team-social-container bg-primary py-2">
+                        <div class="team-social w-max mx-auto">
+                            <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#!"><i class="fab fa-instagram"></i></a>
+                            <a href="#!"><i class="fab fa-twitter"></i></a>
+                            <a href="#!"><i class="fab fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
-
-<?php endif; ?>
