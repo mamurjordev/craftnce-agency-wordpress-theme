9f1e2f265ca32954dc94417230790368ebf10089
saidmsaif <saidmsaiff@gmail.com>
2022-02-08 15:38:52 +0600

    add controls for select footer widget layout

--- inc/option-panel/customizer/options/option-footer.php
+++ inc/option-panel/customizer/options/option-footer.php
@@ -13,8 +13,10 @@
         'capability'        => 'edit_theme_options',
         'transport'         => 'refresh',
         'type'              => 'theme_mod',
-        'sanitize_callback' => function( $input ) {
-            return ( ( isset( $input ) && true == $input ) ? true : false );
+        'sanitize_callback' => function( $input, $setting ){
+            $input = sanitize_key($input);
+            $choices = $setting->manager->get_control( $setting->id )->choices;
+            return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
         }
     ));
     $wp_customize->add_control('craftnce_footer_widget_section_ctrl', array(
@@ -23,6 +25,59 @@
         'settings'          =>  'craftnce_footer_widget_section_setting',
         'type'              =>  'checkbox'
     ));
+
+    /**
+     * Footer Widget Section - Layout Fix
+     */
+    $wp_customize->add_setting('craftnce_footer_widget_upper_section_setting', array(
+        'default'           =>  'col_6',
+        'capability'        =>  'edit_theme_options',
+        'transport'         =>  'refresh',
+        'type'              =>  'theme_mod',
+        'sanitize_callback' =>  function( $input, $setting ){
+            $input = sanitize_key($input);
+            $choices = $setting->manager->get_control( $setting->id )->choices;
+            return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
+        }
+    ));
+
+    $wp_customize->add_control( 'craftnce_footer_widget_upper_section_ctrl', array(
+        'type'          => 'select',
+        'label'         => __( 'Select Upper Widget Layout', 'craftnce' ),
+        'section'       => 'craftnce_footer',
+        'settings'      =>  'craftnce_footer_widget_upper_section_setting',
+        'description'   => __( 'Select layout', 'craftnce' ),
+        'choices'       => array(
+            'col_6'             => __( 'Column - 6', 'craftnce' ),
+            'col_4'             => __( 'Column - 4', 'craftnce' ),
+            'col_2'             => __( 'Column - 2', 'craftnce' ),
+        ),
+    ) );
+
+    $wp_customize->add_setting('craftnce_footer_widget_down_section_setting', array(
+        'default'           =>  'col_3',
+        'capability'        =>  'edit_theme_options',
+        'transport'         =>  'refresh',
+        'type'              =>  'theme_mod',
+        'sanitize_callback' =>  function( $input, $setting ){
+            $input = sanitize_key($input);
+            $choices = $setting->manager->get_control( $setting->id )->choices;
+            return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
+        }
+    ));
+
+    $wp_customize->add_control( 'craftnce_footer_widget_down_section_ctrl', array(
+        'type'          => 'select',
+        'label'         => __( 'Select Down Widget Layout', 'craftnce' ),
+        'section'       =>  'craftnce_footer',
+        'settings'      =>  'craftnce_footer_widget_down_section_setting',
+        'description'   => __( 'This is a custom select option.', 'craftnce' ),
+        'choices'       => array(
+            'col_3'             => __( 'Column - 3', 'craftnce' ),
+            'col_2'             => __( 'Column - 2', 'craftnce' ),
+            'col_1'             => __( 'Column - 1', 'craftnce' ),
+        ),
+    ) );
 
     /**
      * Copyright Text
