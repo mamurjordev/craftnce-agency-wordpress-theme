f39b2f5f57a3eefd1b57fe1e2dfe6b135ebe777a
saidmsaif <saidmsaiff@gmail.com>
2022-02-08 10:40:05 +0600

    first team section dynamic

--- template-parts/common/section-team.php
+++ template-parts/common/section-team.php
@@ -14,23 +14,54 @@
             <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
                 <div class="team-member-card card border-0 overflow-hidden d-flex flex-column justify-content-between">
                     <div class="team-avatar text-center">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/images.png"
+                        <img 
+                            src="<?php
+                                    echo esc_url(get_theme_mod('craftnce_team_member_1_avatar_settings') ?
+                                    get_theme_mod('craftnce_team_member_1_avatar_settings') : get_template_directory_uri().'/assets/img/icons/images.png');
+                                ?>" 
                             class="img-fluid team-avatar-img rounded-circle mx-auto" alt="">
                     </div>
 
                     <div class="text-center">
-                        <h3 class="fw-bold">Hasan Ali</h3>
-                        <h6>Founder & Chairman</h6>
+                        <h3 class="fw-bold">
+                            <?php
+                                echo esc_html(get_theme_mod('craftnce_team_member_1_name_settings', 'Hasan Ali'));
+                            ?>
+                        </h3>
+                        <h6>
+                            <?php
+                                echo esc_html(get_theme_mod('craftnce_team_member_1_designation_settings', 'Web Developer'));
+                            ?>
+                        </h6>
                     </div>
 
+                    <?php
+                        $craftnce_team_member_social_media_repeater = get_theme_mod('craftnce_team_social_media_repeater_item_settings');
+                        $craftnce_team_member_social_media_repeater_decoded = json_decode($craftnce_team_member_social_media_repeater);
+                        if(!empty($craftnce_team_member_social_media_repeater_decoded)) :
+                    ?>
                     <div class="team-social-container bg-primary py-2">
                         <div class="team-social w-max mx-auto">
-                            <a href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#!"><i class="fab fa-instagram"></i></a>
-                            <a href="#!"><i class="fab fa-twitter"></i></a>
-                            <a href="#!"><i class="fab fa-whatsapp"></i></a>
+                            <?php foreach($csm as $m) : ?>
+                            <a href="<?php echo esc_url( $m->title ); ?>">
+                                <i class="<?php echo esc_attr( $m->text ); ?>"></i>
+                            </a>
+                            <?php endforeach; ?>
                         </div>
                     </div>
+                    <?php
+                        else:
+                    ?>
+                        <div class="team-social-container bg-primary py-2">
+                            <div class="team-social w-max mx-auto">
+                                <a href="#">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </div>
+                        </div>
+                    <?php
+                        endif;
+                    ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4 col-xxl-3 mt-4">
