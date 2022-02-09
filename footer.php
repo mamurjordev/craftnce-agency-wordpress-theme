1a2c30cafde2816100c4b031835abc2e086a439c
saidmsaif <saidmsaiff@gmail.com>
2022-02-09 15:35:57 +0600

    add condition for footer panel

--- footer.php
+++ footer.php
@@ -2,21 +2,26 @@
         <div class="footer-widget-wrapper py-5">
             <div class="container">
                 <?php
-                    // Upper Panel
-                    if('col_6' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/six-quarter');
-                    } elseif('col_4' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/quarter');
-                    } elseif('col_2' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/cols-2');
+                    if(get_theme_mod('craftnce_footer_widget_upper_section_show_setting', 1)) {
+                        // Upper Panel
+                        if('col_6' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/six-quarter');
+                        } elseif('col_4' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/quarter');
+                        } elseif('col_2' === get_theme_mod('craftnce_footer_widget_upper_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/cols-2');
+                        }
                     }
-// Bottom Panel
-                    if('col_b_3' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/bottom-3-panel');
-                    } elseif('col_b_2' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/bottom-2-panel');
-                    } elseif('col_b_1' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
-                        get_template_part('/template-parts/footer/bottom-1-panel');
+
+                    if(get_theme_mod('craftnce_footer_widget_bottom_section_show_setting', 1)) {
+                        // Bottom Panel
+                        if('col_b_3' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/bottom-3-panel');
+                        } elseif('col_b_2' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/bottom-2-panel');
+                        } elseif('col_b_1' === get_theme_mod('craftnce_footer_widget_bottom_section_layout_setting')) {
+                            get_template_part('/template-parts/footer/bottom-1-panel');
+                        }
                     }
                 ?>
             </div>
