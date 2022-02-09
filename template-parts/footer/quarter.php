5895aef8e4e80607290e08821702b83f1557ec6c
saidmsaif <saidmsaiff@gmail.com>
2022-02-09 12:07:08 +0600

    footer upper layout panel files

---
+++ template-parts/footer/quarter.php
@@ -0,0 +1,36 @@
+<div class="row row-cols-2 row-cols-md-3 row-cols-xl-4">
+    <div class="col mt-4">
+        <h6 class="text-light">Hosting</h6>
+        <ul class="list-unstyled footer-menu">
+            <li><a href="">Web Hosting</a></li>
+            <li><a href="">Turbo Hosting</a></li>
+            <li><a href="">Reseller Hosting</a></li>
+        </ul>
+    </div>
+    <div class="col mt-4">
+        <h6 class="text-light">Hosting</h6>
+        <ul class="list-unstyled footer-menu">
+            <li><a href="">Web Hosting</a></li>
+            <li><a href="">Turbo Hosting</a></li>
+            <li><a href="">Reseller Hosting</a></li>
+        </ul>
+    </div>
+    <div class="col mt-4">
+        <h6 class="text-light">Hosting</h6>
+        <ul class="list-unstyled footer-menu">
+            <li><a href="">Web Hosting</a></li>
+            <li><a href="">Turbo Hosting</a></li>
+            <li><a href="">Reseller Hosting</a></li>
+        </ul>
+    </div>
+    <div class="col mt-4">
+        <h6 class="text-light">Hosting</h6>
+        <ul class="list-unstyled footer-menu">
+            <li><a href="">Web Hosting</a></li>
+            <li><a href="">Turbo Hosting</a></li>
+            <li><a href="">Reseller Hosting</a></li>
+        </ul>
+    </div>
+</div>
+
+<hr class="footer-bottom-line">
