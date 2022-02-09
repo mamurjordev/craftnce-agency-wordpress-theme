65aa48cce565b92e2cf0ee880b906388b0f02dda
saidmsaif <saidmsaiff@gmail.com>
2022-02-09 15:37:16 +0600

    add footer bottom panel file

---
+++ template-parts/footer/bottom-1-panel.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col mt-4 footer-address-area">
+        <?php
+            if(is_active_sidebar( 'footer_bottom_first_widget_area' )) {
+                dynamic_sidebar( 'footer_bottom_first_widget_area' );
+            }
+        ?>
+    </div>
+</div>
---
+++ template-parts/footer/bottom-2-panel.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-6 mt-4 footer-address-area">
+        <?php
+            if(is_active_sidebar( 'footer_bottom_first_widget_area' )) {
+                dynamic_sidebar( 'footer_bottom_first_widget_area' );
+            }
+        ?>
+    </div>
+
+    <div class="col-6 mt-4">
+        <?php
+            if(is_active_sidebar( 'footer_bottom_second_widget_area' )) {
+                dynamic_sidebar( 'footer_bottom_second_widget_area' );
+            }
+        ?>
+    </div>
+</div>
